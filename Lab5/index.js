7680c941632be58879144d0b34c668f549737354
mscMstr <delgadocarla2021@gmail.com>
2025-04-02 23:28:31 -0400

    fixed broken import

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,4 +1,4 @@
-import Module from "./module.js";
+import Module from "./Module.js";
 import PathParameters from "./PathParamters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
